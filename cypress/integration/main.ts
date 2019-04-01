5a81e910cd6eba4cde2e0e0d56a991093fcb1268
kds <kds@automama.ru>
2019-04-01 18:32:07 +0300

    Текущие изменения .

--- cypress/integration/main.ts
+++ cypress/integration/main.ts
@@ -18,31 +18,31 @@ describe('Главная', () => {
             .route('https://test.automama.ru/api/v2/auctions/search?*').as('getSearch')
             .visit(urls.mainPage.main)
     })
-    // it('тултипы УТП', () => {
-    //     cy.isTooltipsOpenAfterMousmoove(mainPage.utpsText)
-    // })
-    // it('переход в статьи', () => {
-    //     cy.get(mainPage.news.link).click().url().should('contains', '/blog')
-    // })
-    // it('открытие блока инфо подробнее', () => {
-    //     cy.blockIsOpenAfterClick(mainPage.seoText.showMore, mainPage.seoText.link)
-    // })
-    // it('переход по якорной ссылке в каталог', () => {
-    //     cy.get(mainPage.seoText.showMore).click()
-    //         .get(mainPage.seoText.link).click().url().should('contains', '/cars')
-    // })
-    // it('при наведении на карточку появилась инфо', () => {
-    //     cy.get(filterObject.carItem.auctionItemsResult).first().trigger('mouseenter')
-    //         .get(mainPage.carItems.info).should('be.visible')
-    // })
-    // it('переход в detail-car по клику на auction-item', () => {
-    //     cy.get(filterObject.carItem.auctionItemsResult).first().click()
-    //         .url().should('contains', '/car/')
-    // })
+    it('тултипы УТП', () => {
+        cy.isTooltipsOpenAfterMousmoove(mainPage.utpsText)
+    })
+    it('переход в статьи', () => {
+        cy.get(mainPage.news.link).click().url().should('contains', '/blog')
+    })
+    it('открытие блока инфо подробнее', () => {
+        cy.blockIsOpenAfterClick(mainPage.seoText.showMore, mainPage.seoText.link)
+    })
+    it('переход по якорной ссылке в каталог', () => {
+        cy.get(mainPage.seoText.showMore).click()
+            .get(mainPage.seoText.link).click().url().should('contains', '/cars')
+    })
+    it('при наведении на карточку появилась инфо', () => {
+        cy.get(filterObject.carItem.auctionItemsResult).first().trigger('mouseenter')
+            .get(mainPage.carItems.info).should('be.visible')
+    })
+    it('переход в detail-car по клику на auction-item', () => {
+        cy.get(filterObject.carItem.auctionItemsResult).first().click()
+            .url().should('contains', '/car/')
+    })
     filterSpec.isFilterWorking()
-    // paginationSpec.isPaginationWorking(
-    //     mainPage.pagination.container,
-    //     mainPage.pagination.results,
-    //     filterObject.carItem.auctionItemsResult)
-    // reviewGallerySpec.isReviewGalleryWorking(mainPage.reviewGalleryContainer)
+    paginationSpec.isPaginationWorking(
+        mainPage.pagination.container,
+        mainPage.pagination.results,
+        filterObject.carItem.auctionItemsResult)
+    reviewGallerySpec.isReviewGalleryWorking(mainPage.reviewGalleryContainer)
 })
