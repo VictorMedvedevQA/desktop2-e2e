1e1e2680efb8443bb0e47371aef755fd9ada5e7a
kds <kds@automama.ru>
2019-09-23 19:10:58 +0300

    неиспользуемый файл

--- cypress/integration/express-detail-car.ts
+++ cypress/integration/express-detail-car.ts
@@ -1,12 +1,10 @@
 import { AuthPage } from '../pages/auth/auth.page';
 import { ExpressCatalogPage } from '../pages/express-catalog/express-catalog.page';
-import { ExpressDetailCarPage } from '../pages/express-detail-car/express-detail-car';
 import { FormTestingObject } from '../shared/form-testing/form-testing.object';
 import { urls } from '../support/urls';
 
 const expressCatalogPage = new ExpressCatalogPage();
 const authPage = new AuthPage();
-const expressDetailCarPage = new ExpressDetailCarPage();
 const formTestingObject = new FormTestingObject();
 
 describe('Детальная страница авто в экспресс-аукционах ', () => {
